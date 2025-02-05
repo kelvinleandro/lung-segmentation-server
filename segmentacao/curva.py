fb15580f98db81d23138f16f4e9c0100f20b1c48
gabrielfruet <gabrielfruet538@gmail.com>
2025-02-05 13:30:16 -0300

    fix: os pontos da curva devem ser sempre maiores que 0

--- segmentacao/curva.py
+++ segmentacao/curva.py
@@ -16,8 +16,12 @@ def inicializa_curva(
         quantidade_pixels: quantidade de pontos que a curva terá
     Return:
         curva: lista de pontos que representam a curva
+    Raises:
+        AssertionError: caso curva seja menor que 0 em algum ponto
     """
     angulos = np.linspace(0, 2 * np.pi, quantidade_pixels)
     curva = ponto + np.c_[np.cos(angulos), np.sin(angulos)] * raio
+
+    assert np.all(curva > 0), "Os pontos da curva devem sempre ser maior que 0"
 
     return curva.astype(np.int16)
