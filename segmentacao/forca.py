54ff3f955b289a3cf168477927d4fbe58cda3c03
Davi0Cruz <davilcruz@hotmail.com>
2025-02-06 07:41:52 -0300

    feat: força de continuidade

---
+++ segmentacao/forca.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def forca_continuidade(pontos: np.array, indice: int) -> np.float64:
+    """
+    Calcula a força de continuidade da curva em um ponto.
+
+    Args:
+        pontos: np.array - Pontos da curva.
+        indice: int - Índice do ponto.
+    Return:
+        float - Força de continuidade.
+    """
+
+    # Calcular a distância média entre os pontos
+    DM = np.linalg.norm(pontos - np.roll(pontos, 1, axis=0), axis=1).mean()
+
+    # Calcular a derivada discreta do ponto
+    dc = np.linalg.norm(pontos[indice] - pontos[indice - 1])
+
+    return np.abs(DM - dc)
+
