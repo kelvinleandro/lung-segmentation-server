ecc8cbbc4191e0888c87a8df488d8f57f2d56635
rodrigohiury <rodrigohiury@alu.ufc.br>
2025-03-06 22:42:23 -0300

    ruff format e comentários no código

--- api/app.py
+++ api/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, send_file
-from flask_cors import CORS  
+from flask_cors import CORS
 import pydicom
 import numpy as np
 import io
 
 app = Flask(__name__)
-CORS(app,origins=["http://localhost:8000"])  
+CORS(app,origins=["http://localhost:8000"])
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -18,7 +18,7 @@ def upload():
 
     # Simples processamento (inversão de intensidade como exemplo)
     dicom_data.PixelData = (np.max(dicom_data.pixel_array) - dicom_data.pixel_array).tobytes()
-    
+
     # Salvar em memória
     output = io.BytesIO()
     dicom_data.save_as(output)
@@ -30,9 +30,11 @@ def upload():
         print("Assinatura DICOM válida encontrada!")
     else:
         print("Erro: arquivo salvo no buffer pode estar corrompido.")
-    
+
     output.seek(0)
-    return send_file(output, mimetype='application/dicom', as_attachment=True, download_name="processed.dcm")
-    
+    return send_file(output, mimetype='application/dicom',
+                     as_attachment=True,
+                     download_name="processed.dcm")
+
 if __name__ == '__main__':
     app.run(debug=True)
--- segmentacao/lim_global_simples.py
+++ segmentacao/lim_global_simples.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-from carregar import carregar_imagem
+
 
 def lim_global_simples(img: np.ndarray) -> np.ndarray:
     """
@@ -9,24 +9,34 @@ def lim_global_simples(img: np.ndarray) -> np.ndarray:
     args:
         img: np.ndarray - Imagem em tons de cinza
     return:
-        np.ndarray - Imagem limiasrizada
+        np.ndarray - Imagem limiarizada
     """
-    img = cv2.GaussianBlur(img, (5,5), 0)
-    img_bin = np.zeros_like(img)
-    _, img_bin = cv2.threshold(img, -500, 255, cv2.THRESH_BINARY)
-    img_bin = np.uint8(img_bin * 255)
+
+    # imagem é filtrada com um filtro gaussiano
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+
+    # novas imagens são criadas
     img = np.zeros_like(img)
-    img = img.astype(np.uint8)
+    img_bin = np.zeros_like(img)
+
+    _, img_bin = cv2.threshold(img, -500, 255, cv2.THRESH_BINARY)  # aplicada limiar
+    img_bin = np.uint8(img_bin * 255)
+    img = img.astype(np.uint8)  # construção de nova imagem
+
+    # contornos traçados
     contornos, _ = cv2.findContours(img_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # contornos filtrados
     contornos_filtrados = [cnt for cnt in contornos if cv2.contourArea(cnt) > 1200]
+    contornos_filtrados = sorted(contornos_filtrados, key=cv2.contourArea, reverse=True)
+
+    # contorno do corpo é removido
+    if len(contornos_filtrados) > 2:
+        contornos_filtrados = contornos_filtrados[1:3]
+
+    # contorno é desenhado na imagem
     img_limiarizada = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    cv2.drawContours(img_limiarizada, contornos_filtrados, -1, (0, 0, 255), 1)
+    cv2.drawContours(img_limiarizada, contornos_filtrados, -1, (255, 0, 0), 1)
+    img_limiarizada = cv2.cvtColor(img_limiarizada, cv2.COLOR_BGR2GRAY)
+
     return img_limiarizada
-
-
-img = carregar_imagem('data/pulmao2/88.dcm')
-img_lim = lim_global_simples(img)
-cv2.imshow('Imagem Normal', img)
-cv2.imshow('Imagem Limiarizada', img_lim)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
